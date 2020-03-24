a8b5ed03aa29ddda65c72c496f1543c490d6beba
jfiedorowicz <jfiedorowicz@GMAIL.COM>
2020-03-24 22:21:08 +0100

    changed something

---
+++ PythonApplication1/module1.py
@@ -0,0 +1,7 @@
+import def parse_args(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description="My Python script.")
+
+    # Use the arg_* snippets to add arguments
+    
+
+    return parser.parse_args(argv)
